618dd2238141d09361bd7f57e9e9e3edbf5292fe
Dylan Meeus <meeusdylan@hotmail.com>
2018-04-07 04:28:15 +0200

    Show words on frontend

--- Frontend/TypingPage/world.js
+++ Frontend/TypingPage/world.js
@@ -1,23 +1,31 @@
 
 // bootstrap app to the loading of the page (it doesn't work as shown in vue their examples)
 
-var url = "http://localhost:8080/words"
+var url = "http://localhost:8080/words";
+var loadedWords = [];
 window.onload = function() {
     var app = new Vue({
         el: "#app",
         data: {
-            message: "Hello World!"
+            wordlist: "Hello World!"
         },
         methods: {
             loadWords: function () {
                 // Ping the server for some sweet words!
                 fetch(url, {
                     headers: {
-                        'content-type' : 'application/json'
+                        'content-type': 'application/json'
                     },
                     method: "get",
                     mode: "cors"
-                }).then(response => console.log(response));
+                }).then(
+                    response => response.json()
+                ).then(arr =>{
+                        // apperantly Jackson or Javascript removes the quotes.
+                        loadedWords = arr;
+                        this.wordlist = arr.join(" ");
+                    }
+                );
             }
         },
         beforeMount() {
